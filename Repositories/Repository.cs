5e6b104585f1721fecbb2f625baf35d46bd007fe
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-05-15 14:50:22 -0600

    Clase repositorio avanzada

---
+++ Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using ApiJerarquia.Models.Entities;
+
+namespace ApiJerarquia.Repositories
+{
+    public class Repository<T> where T : class
+    {
+        public ItesrcneActividadesContext Context { get; }
+        public Repository(ItesrcneActividadesContext context)
+        {
+            Context = context;
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+        public virtual T? Get(object id)
+        {
+            return Context.Find<T>(id);
+        }
+        public virtual void Insert(T entity)
+        {
+            Context.Add(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Update(T entity)
+        {
+            Context.Update(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Delete(object id)
+        {
+            var entity = Get(id);
+            if(entity != null)
+            {
+                Delete(entity);
+            }
+        }
+        public virtual void Delete(T entity)
+        {
+            Context.Remove(entity);
+            Context.SaveChanges();
+        }
+    }
+}
