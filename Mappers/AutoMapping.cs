7e50ff911c311e6f73f692a81b79f2d4e2bec570
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-05-16 15:20:00 -0600

    Mapper Terminado

--- Mappers/AutoMapping.cs
+++ Mappers/AutoMapping.cs
@@ -1,6 +1,7 @@
 ﻿using ApiJerarquia.Models.Entities;
 using AutoMapper;
 using ApiJerarquia.Models.DTOs;
+using Microsoft.Extensions.Options;
 namespace ApiJerarquia.Mappers
     
 {
@@ -9,9 +10,18 @@ namespace ApiJerarquia.Mappers
         public AutoMapping() 
         {
             CreateMap<Actividades, ActividadDTO>()
-                .ForMember(destinationMember=>destinationMember.Titulo);
+                .ForMember(destination=>destination.Titulo, opt=>opt.MapFrom(soruce=>soruce.Titulo))
+                .ForMember(destination=>destination.Departamento, opt=>opt.MapFrom(source=>source.IdDepartamentoNavigation.Nombre))
+                .ForMember(destination=>destination.Id,opt=>opt.MapFrom(source=>source.Id))
+                .ForMember(destination=>destination.Descripcion, opt=>opt.MapFrom(source=>source.Descripcion))
+                .ForMember(destination=>destination.FechaActualizacion,opt=>opt.MapFrom(source=>source.Descripcion))
+                .ForMember(destination=>destination.FechaRealizacion, opt=>opt.MapFrom(source=>source.FechaRealizacion))
+                .ForMember(destination=>destination.FechaCreacion,opt=>opt.MapFrom(source=>source.FechaCreacion));
             CreateMap<ActividadDTO, Actividades>();
-            CreateMap<Departamentos,DepartamentoDTO>();
+            CreateMap<Departamentos,DepartamentoDTO>()
+                .ForMember(x=>x.Id,opt=>opt.MapFrom(source=>source.Id))
+                .ForMember(x=>x.Nombre,opt=>opt.MapFrom(source=>source.Nombre))
+                .ForMember(x=>x.DepartamentoSuperior,opt=>opt.MapFrom(source=>source.IdSuperiorNavigation.Nombre));
             CreateMap<DepartamentoDTO, Departamentos>();
         }
 
